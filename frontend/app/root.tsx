75facb0399fc0e29e7d522fe1e1b3f140123466d
yamao-sys <yamaopanku@gmail.com>
2024-08-25 17:51:07 +0900

    action

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -14,19 +14,24 @@ import invariant from "tiny-invariant";
 
 import "./tailwind.css";
 
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
 
-export const loader = async ({ params }: LoaderFunctionArgs) => {
-  invariant(params.contactId, "Missing contactId param");
-  const contact = await getContacts(params.contactId);
-  if (!contact) {
-    throw new Response("Not Found", { status: 404 });
-  }
+// export const loader = async ({ params }: LoaderFunctionArgs) => {
+//   invariant(params.contactId, "Missing contactId param");
+//   const contact = await getContacts(params.contactId);
+//   if (!contact) {
+//     throw new Response("Not Found", { status: 404 });
+//   }
+//   return json({ contact });
+// }
+
+export const action = async () => {
+  const contact = await createEmptyContact();
   return json({ contact });
 }
 
 export default function App() {
-  const { contact } = useLoaderData<typeof loader>();
+  // const { contact } = useLoaderData<typeof loader>();
 
   return (
     <html lang="en">
